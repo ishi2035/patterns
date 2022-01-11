6ff17e803ece5f35fd072d09a9eafdbc96dec865
ishi2035 <73154460+ishi2035@users.noreply.github.com>
2022-01-11 12:44:43 +0530

    pattern 3
    *  *  *  *  *  
    *  *  *  *
    *  *  *
    *  *
    *

--- pattern.cpp
+++ pattern.cpp
@@ -1,10 +1,10 @@
 /*
 
-        * 
-      * *
-    * * *
-  * * * *
-* * * * *
+*  *  *  *  *  
+*  *  *  *
+*  *  *
+*  *
+*
 
 */
 
@@ -13,12 +13,10 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=n;j>i;j--)
-            cout<<"  ";
-        for(int j=0;j<i;j++){
-            cout<<"* ";
-        }
+    for(int i=n;i>0;i--){
+        for(int j=i;j>0;j--)
+            cout<<"*  ";
+        
         cout<<endl;
     }
 }
