0432699e5b5c2ef996b7696976bd45667d77191c
ishi2035 <73154460+ishi2035@users.noreply.github.com>
2022-01-11 20:35:12 +0530

    solid Diamond Star Pattern
         * 
        * *
       * * *
      * * * *
     * * * * *
    * * * * * *
     * * * * *
      * * * *
       * * *
        * *
         *

---
+++ pattern5.cpp
@@ -0,0 +1,44 @@
+/*
+     * 
+    * *
+   * * *
+  * * * *
+ * * * * *
+* * * * * *
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
+
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int k=1;
+    
+    for(int i=0;i<n;i++){
+
+        for(int j=n-i;j>0;j--)
+            cout<<" ";
+        for(int j=0;j<=i;j++){
+                cout<<"* ";
+                k++;
+        }
+         cout<<endl;
+    }
+    for(int i=n;i>=0;i--){   
+        for(int j=n-i;j>0;j--) 
+            cout<<" ";
+        for(int j=0;j<=i;j++){ 
+                cout<<"* ";
+                k++;
+        }
+         cout<<endl;
+    }
+        
+}
